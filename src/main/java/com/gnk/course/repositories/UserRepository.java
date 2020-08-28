a9812a0f3899ee6de090dbf681e5bd011452d6ca
gabrielNunesKlein <gabrielgnk@outlook.com>
2020-08-28 20:03:40 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/gnk/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gnk.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gnk.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
