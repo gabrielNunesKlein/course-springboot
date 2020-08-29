d2fda50010fcead2a993a6445490a312b87757b6
gabrielNunesKlein <gabrielgnk@outlook.com>
2020-08-29 16:04:10 -0300

    Product

---
+++ src/main/java/com/gnk/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.gnk.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gnk.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
