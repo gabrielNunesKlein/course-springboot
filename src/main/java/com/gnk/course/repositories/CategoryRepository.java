130bd5f7fda8df93b335ed6379e5de688b67f42d
gabrielNunesKlein <gabrielgnk@outlook.com>
2020-08-30 13:32:20 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/gnk/course/repositories/CategoryRepository.java
+++ src/main/java/com/gnk/course/repositories/CategoryRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gnk.course.entities.Category;
 
+
 public interface CategoryRepository extends JpaRepository<Category, Long>{
 
 }
